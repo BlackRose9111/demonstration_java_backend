093dfd37ec70f1060a9be3d88d4473ca83a88d77
Black Rose <86892804+BlackRose9111@users.noreply.github.com>
2024-02-19 13:59:23 +0300

    default project with no changes so far

---
+++ src/main/java/com/baran/demonstration_java_backend/DemonstrationJavaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.baran.demonstration_java_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemonstrationJavaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemonstrationJavaBackendApplication.class, args);
+	}
+
+}
