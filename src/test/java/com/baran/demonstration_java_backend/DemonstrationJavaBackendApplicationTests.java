fea888185ffc0362d7827937cefd5d7d76c535d7
Black Rose <86892804+BlackRose9111@users.noreply.github.com>
2024-02-19 13:58:55 +0300

    Init

---
+++ src/test/java/com/baran/demonstration_java_backend/DemonstrationJavaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baran.demonstration_java_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemonstrationJavaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
